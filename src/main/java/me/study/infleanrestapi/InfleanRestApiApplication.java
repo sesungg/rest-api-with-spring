81bd2d03cb212750887e8813d04155f8a1be4fca
sesung <sesungg@gmail.com>
2024-09-19 23:58:32 +0900

    first commit

---
+++ src/main/java/me/study/infleanrestapi/InfleanRestApiApplication.java
@@ -0,0 +1,13 @@
+package me.study.infleanrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfleanRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfleanRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/study/infleanrestapi/InfleanRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.infleanrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfleanRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
